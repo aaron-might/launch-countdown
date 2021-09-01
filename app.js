ab9ea52fec25caf6f2136b3e40fe29328b72def4
aaron-might <aaronmighty7@gmail.com>
2021-09-01 03:29:00 -0400

    writing function timedays

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const countdown = () => {
-const date= new Date('September 30, 2021 00:00:00');
-console.log(date)
+const endDate= new Date('September 30, 2021 00:00:00').getTime();
+// console.log(date)
+const now = new Date().getTime();
+// console.log(now)
+const difference = endDate - now;
+console.log(difference)
+
+const seconds = 1000
+const minutes = seconds * 60
+const hours = minutes *60
+const days = hours * 24
+
+const timeDays = Math.floor(difference / days)
+console.log(timeDays)
 }
 countdown()
